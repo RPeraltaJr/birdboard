085ca273eb907938cb59e0d4f80b59869097f617
Rafael Peralta Jr <rperaltajr@gmail.com>
2019-09-08 22:39:56 -0400

    Prepare the frontend

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,22 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Birdboard</h1>
+@extends('layouts.app')
 
-    <ul>
-        @forelse($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        @empty 
-            <li>No projects yet</li>
-        @endforelse
-    </ul>
-</body>
-</html>
+@section('content')
+
+<div class="flex items-center mb-3">
+    <a href="/projects/create">New Project</a>
+</div>
+
+<ul>
+    @forelse($projects as $project)
+        <li>
+            <a href="{{ $project->path() }}">{{ $project->title }}</a>
+        </li>
+    @empty 
+        <li>No projects yet</li>
+    @endforelse
+</ul>
+
+@endsection
