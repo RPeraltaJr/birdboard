003d7e13843b58ab1d99691e67d98a96732199ef
Rafael Peralta Jr <rafaelp@bayardad.com>
2019-09-10 14:38:12 -0400

    Update styles

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,29 +3,20 @@
 @section('content')
 
     <header class="flex items-center mb-3 py-4">
-        <div class="flex justify-between items-center w-full">
+        <div class="flex justify-between items-end w-full">
             <h2 class="text-gray-600 text-sm font-normal">My Projects</h2>
             <a href="/projects/create" class="button">New Project</a>
         </div>
     </header>
 
-    <main class="lg:flex lg:flex-wrap -mx-3">
+    <main class="lg:flex lg:flex-wrap lg:-mx-3">
         @forelse($projects as $project)
-        <div class="lg:w-1/3 px-3 pb-6">
-            <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
-                <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue pl-4">
-                    <a href="{{ $project->path() }}" class="text-black no-underline">
-                        {{ $project->title }}
-                    </a>
-                </h3>
-                <div class="text-gray-600">{{ str_limit($project->description, 100) }}</div>
+            <div class="lg:w-1/3 px-3 pb-6">
+                @include ('projects.card')
             </div>
-        </div>
         @empty 
             <div>No projects yet</div>
         @endforelse
     </main>
-
-    <!-- {{ $project->title }}</a> -->
 
 @endsection
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,8 +2,41 @@
 
 @section('content')
 
-<h1>{{ $project->title }}</h1>
-<p>{{ $project->description }}</p>
-<a href="/projects">Go Back</a>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-end w-full">
+            <p class="text-gray-600 text-sm font-normal">
+                <a href="/projects" class="text-gray-600 text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+            </p>
+            <a href="/projects/create" class="button">New Project</a>
+        </div>
+    </header>
 
+    <main>
+        <div class="lg:flex lg:-mx-3">
+
+            <div class="lg:w-3/4 px-3 mb-6">
+                <div class="mb-8">
+                    <h2 class="text-lg text-gray-500 font-normal mb-3">Tasks</h2>
+
+                    {{-- tasks --}}
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card mb-3">Lorem ipsum.</div>
+                    <div class="card">Lorem ipsum.</div>
+                </div>
+                <div class="mb-8">
+                    <h2 class="text-lg text-gray-500 font-normal mb-3">General Notes</h2>
+
+                    {{-- general notes --}}
+                    <textarea class="card w-full" style="min-height: 200px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero nesciunt in enim. Eligendi adipisci labore qui, facilis aperiam quod ipsum dolore, fugit ab totam reprehenderit voluptatum in corrupti aliquam vel.</textarea>
+                </div>
+            </div>
+
+            <div class="lg:w-1/4 px-3">
+                @include ('projects.card')
+            </div>
+            
+        </div>
+    </main>
+ 
 @endsection
